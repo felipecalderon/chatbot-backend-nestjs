7c526b2321a684fec1c7f53b0032dd40ad4df721
Felipe <felipe.calderon321@gmail.com>
2025-08-07 12:03:17 -0400

    Add Docker support with Dockerfile, docker-compose, and Jenkins pipeline; configure CORS and validation in main application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para permitir solicitudes desde cualquier origen.
+  app.enableCors({
+    origin: '*', // O un array de dominios: ['http://example.com']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Usar un ValidationPipe global para validar automáticamente los DTOs.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // Lanza un error si se reciben propiedades no esperadas
+      transform: true, // Transforma el payload a una instancia del DTO
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
